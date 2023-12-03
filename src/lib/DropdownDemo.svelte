beb3ab62c20b6f9784158795ec8ca401eec047a7
3n3a <46775561+3n3a@users.noreply.github.com>
2023-12-03 17:05:16 +0100

    Update DropdownDemo.svelte

--- src/lib/DropdownDemo.svelte
+++ src/lib/DropdownDemo.svelte
@@ -2,11 +2,16 @@
   import AutoComplete from 'simple-svelte-autocomplete'
 
   const loadSuggestions = async (query: string, limit: number) => {
-    const headers = {
-      "apikey": "uEzad5RVG9e9967xypBS8sFzKRadEtCV",
-      "accept": "application/json"
+    const options = {
+      method: "GET",
+      mode: "cors",
+      credentials: "include",
+      headers: {
+        "apikey": "uEzad5RVG9e9967xypBS8sFzKRadEtCV",
+        "accept": "application/json"
+      },
     }
-    const res = await fetch(`https://api.3n3a.ch/plz/search?q=${query}`, {mode: "cors", headers: headers})
+    const res = await fetch(`https://api.3n3a.ch/plz/search?q=${query}`, options)
     const data = await res.json()
     return data
   }
